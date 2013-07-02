cb81fba38502bd8ca337bcf5ecd73ddf1d80b001
Paweł Pelczar <paweusz@mint>
2013-07-02 14:29:54 +0200

    Angular scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('radioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
