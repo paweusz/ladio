232776fe0a03a867bbea55c0f4e8af812c1fde6c
Paweł Pelczar <paweusz@mint>
2013-07-05 13:25:19 +0200

    Function binding in angular directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,9 @@ angular.module('radioApp')
   .controller('MainCtrl', function ($scope, $resource) {
     var Genre = $resource('/genres');
     $scope.genres = Genre.query(function() {
-      console.log('pimpa');
-      
+      console.log('genre query');
     });
+    $scope.play = function() {
+      console.debug('play');
+    };
   });
--- app/scripts/directives/rdGenre.js
+++ app/scripts/directives/rdGenre.js
@@ -4,9 +4,13 @@ angular.module('radioApp')
   .directive('rdGenre', function () {
     return {
       transclude: true,
+      scope: {
+        onClick: '&onClick'
+      },
       templateUrl: 'scripts/directives/rdGenre.html',
       link: function(scope, iElement, iAttrs) {
         console.debug('postLink');
+        iElement.bind('click', scope.onClick);
       }
     }
   });
