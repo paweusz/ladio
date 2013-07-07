c2191c078e1d551720b5a88b9dd50749aa2b8793
Paweł Pelczar <paweusz@mint>
2013-07-07 12:49:13 +0200

    Genres in angular router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,9 @@ angular.module('radioApp', ['ngResource'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
+        redirectTo: '/genres'
+      })
+      .when('/genres', {
         templateUrl: 'views/genres.html',
         controller: 'GenresCtrl'
       })
