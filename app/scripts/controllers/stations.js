61c074616c730ae531326f618c33838a04be23b1
Paweł Pelczar <paweusz@mint>
2013-07-07 23:00:45 +0200

    Playing indicator

--- app/scripts/controllers/stations.js
+++ app/scripts/controllers/stations.js
@@ -3,4 +3,15 @@
 angular.module('radioApp')
   .controller('StationsCtrl', function ($scope, $routeParams, Genres) {
     $scope.stations = Genres.stations($routeParams.genreId, $routeParams.subGenreId);
+    $scope.currentStationId = null;
+
+    $scope.play = function() {
+      $scope.currentStationId = this.station.id;
+      console.debug('play ' + this.currentStationId);
+    };
+
+    $scope.isPlaying = function() {
+      return $scope.currentStationId === this.station.id ? 'playing' : '';
+    };
+
   });
