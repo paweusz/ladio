db95868d845745e52aec6c711843f773ff2ca756
Paweł Pelczar <paweusz@mint>
2013-07-19 19:19:15 +0200

    Fixed pls service and added debug logging

--- angular/app/scripts/services/pls.js
+++ angular/app/scripts/services/pls.js
@@ -4,9 +4,12 @@ angular.module('radioApp')
   .factory('Pls', function($http) {
     return {
       streams: function(url, callbackFn) {
-        return $http.get('http://localhost:9002/streams', {
+        return $http.get('http://localhost:9001/streams', {
             params: {pls: url}
-          }).success(callbackFn)
+          }).success(function(data) {
+            console.debug('Pls fetched.');
+            callbackFn.apply(this, [data]);
+          })
           .error(function(data) {
             console.error('Error fetching pls data. ' + data);
           });
