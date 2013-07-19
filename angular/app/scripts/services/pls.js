284a759f8fdbf8b4c62297ff29b68c9777918a70
Paweł Pelczar <paweusz@mint>
2013-07-19 18:57:53 +0200

    Pls service moved to $http

--- angular/app/scripts/services/pls.js
+++ angular/app/scripts/services/pls.js
@@ -1,16 +1,15 @@
 'use strict';
 
 angular.module('radioApp')
-  .factory('Pls', function($resource) {
-
-    var backend = {
-      host: 'localhost',
-      port: 9001
-    };
-
+  .factory('Pls', function($http) {
     return {
       streams: function(url, callbackFn) {
-        return $resource('http://:host\\::port/streams', backend).query({pls: url}, callbackFn);
+        return $http.get('http://localhost:9002/streams', {
+            params: {pls: url}
+          }).success(callbackFn)
+          .error(function(data) {
+            console.error('Error fetching pls data. ' + data);
+          });
       }
     };
   });
