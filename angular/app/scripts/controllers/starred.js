ddf47ed314578a95911f48976faa8cc15aae8a48
Paweł Pelczar <paweusz@mint>
2013-08-28 21:43:35 +0200

    Sorting starred stations

--- angular/app/scripts/controllers/starred.js
+++ angular/app/scripts/controllers/starred.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('ladioApp')
-  .controller('StarredCtrl', function ($scope, StarredSrv) {
+  .controller('StarredCtrl', function ($scope, StarredSrv, $filter) {
 
-    $scope.stations = StarredSrv.starred();
+    $scope.stations = $filter('orderBy')(StarredSrv.starred(),
+      function(station) {
+        return station.name.trim();
+      });
 
   });
