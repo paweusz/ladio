d8844125749751e3cff55423af5f4f50382da182
Paweł Pelczar <paweusz@mint>
2013-08-31 20:38:36 +0200

    Initial rdPlayer unit test

--- angular/test/spec/directives/rdPlayer.js
+++ angular/test/spec/directives/rdPlayer.js
@@ -2,16 +2,42 @@
 
 describe('Directive: rdPlayer', function () {
   beforeEach(module('ladioApp'));
-
-  var element;
-
-  it('should make hidden element visible', inject(function ($rootScope, $compile) {
-    $rootScope.streams = [];
-    element = angular.element('<audio rd-player="streams"></audio>');
-    element = $compile(element)($rootScope);
+  
+  it('should reset buffer to empty if no streams are provided', 
+      inject(function ($rootScope, rdPlayerDirective) {
+    var streams = [];
+    var scope = $rootScope;
+    var element = {
+      callbacks: {},
+      0: {
+        loadCalled: false,
+        load: function() {
+          this.loadCalled = true;
+        }
+      },
+      bind: function(name, fn) {
+        this.callbacks[name] = fn;
+      },
+      children: function() {
+        return {
+          remove: function() {},
+          bind: function() {}
+        }
+      }
+    };
+    var attrs = {
+      rdPlayer: streams,
+      onplayingstarted: "playingStarted()",
+      onplayingerror: "playingError()",
+      onplayingstalled: "playingStalled()",
+      onplayingresumed: "playingResumed()",
+      maxreconnects: "3"
+    };
     
+    rdPlayerDirective[0].link(scope, element, attrs);
+
     $rootScope.$digest();
     
-    expect(element.text()).toBe('this is the rdPlayer directive');
+    expect(element[0].loadCalled).toBe(true);
   }));
 });
