5f064dd45dbe28acf786487089f79c91aaaacbef
Paweł Pelczar <paweusz@mint>
2013-08-31 21:23:11 +0200

    Test for all streams failed in rdPlayer

--- angular/test/spec/directives/rdPlayer.js
+++ angular/test/spec/directives/rdPlayer.js
@@ -5,9 +5,17 @@ describe('Directive: rdPlayer', function () {
   beforeEach(module('ladioApp'));
 
   var element;
+  var streamsElem;
   var attrs;
   
   beforeEach(function() {
+    streamsElem = {
+      callbacks: {},
+      remove: function() {},
+      bind: function(name, fn) {
+        this.callbacks[name] = fn;
+      }
+    };
     element = {
       callbacks: {},
       0: {
@@ -20,11 +28,9 @@ describe('Directive: rdPlayer', function () {
         this.callbacks[name] = fn;
       },
       children: function() {
-        return {
-          remove: function() {},
-          bind: function() {}
-        }
-      }
+        return streamsElem;
+      },
+      append: function(childElem) {}
     };
     attrs = {
       rdPlayer: "streams",
@@ -46,4 +52,27 @@ describe('Directive: rdPlayer', function () {
     expect(element[0].loadCalled).toBe(true);
 
   }));
+
+  it('should invoke error callback when all streams failed to play', 
+      inject(function ($rootScope, rdPlayerDirective) {
+
+    $rootScope.streams = ['stream1', 'stream2', 'stream3'];
+    rdPlayerDirective[0].link($rootScope, element, attrs);
+    $rootScope.$digest();
+    
+    var errorInvoked = false;
+    $rootScope.playingError = function() {
+      errorInvoked = true;
+    }
+    
+    streamsElem.callbacks['error']();
+    expect(errorInvoked).toBe(false);
+    streamsElem.callbacks['error']();
+    expect(errorInvoked).toBe(false);
+    element[0].networkState = 3;
+    streamsElem.callbacks['error']();    
+    expect(errorInvoked).toBe(true);
+
+  }));
+
 });
