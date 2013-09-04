66bf3a18efcaaa9b7912a26d512389a547f63084
Paweł Pelczar <ppelczar@pgs-soft.com>
2013-09-04 19:14:05 +0200

    Filtering recent stations

--- angular/app/scripts/controllers/recent.js
+++ angular/app/scripts/controllers/recent.js
@@ -1,8 +1,23 @@
 'use strict';
 
 angular.module('ladioApp')
-  .controller('RecentCtrl', function ($scope, Stat) {
+  .controller('RecentCtrl', function ($scope, Stat, $filter) {
 
     $scope.stations = Stat.recentStations();
+    
+    $scope.filterChanged = function() {
+      var filter = $scope.filter;
+      if (!filter || filter === '') {
+        $scope.stations = Stat.recentStations();
+        return;
+      }
+      
+      var regFilter = new RegExp(filter, 'i');
+      $scope.stations = $filter('filter')(Stat.recentStations(),
+        function(station) {
+          return station.name.match(regFilter);
+        });
+
+    };
     
   });
