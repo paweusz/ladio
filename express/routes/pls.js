e22e96d3a6283d6407f69bc4f712246d372f4971
Paweł Pelczar <ppelczar@pgs-soft.com>
2013-09-05 21:58:45 +0200

    Error handling for pls service backend

--- express/routes/pls.js
+++ express/routes/pls.js
@@ -16,26 +16,49 @@ function parsePls(pls) {
 }
 
 function doGetPls(ref, res) {
-  var errHandler = function(e) {
+
+  var errHandler = function(e, rspCode) {
     var msg = 'Error processing pls request (URL: ' + ref + ', msg: ' + e.message + ')';
     console.log(msg);
     console.log(e.stack);
-    res.send(503, msg);
+
+    if (!rspCode) {
+      rspCode = 503;
+    }
+    res.send(rspCode, msg);
   };
+
   http.get(ref, function (httpRsp) {
     var data = '';
 
     httpRsp.on('data', function (chunk) {
       data += chunk;
     });
-      
-    httpRsp.on('end', function () {
-      var streams = parsePls(data);
+    
+    function processRsp(rspData) {
+      var streams = parsePls(rspData);
       var obj = [];
       for (var i = 0; i < streams.length; i++) {
         obj.push({url: streams[i]});
       }
       res.json(obj);
+    }
+      
+    httpRsp.on('end', function () {
+      var statusCode = httpRsp.statusCode;
+      if (statusCode !== 200) {
+        errHandler({
+          message: 'Station pls response ' + statusCode,
+          stack: 'doGetPls(ref, res)'
+        }, statusCode);
+        return;
+      }
+      
+      try {
+        processRsp(data);
+      } catch (e) {
+        errHandler(e);
+      }
     });
 
     httpRsp.on('error', errHandler);
