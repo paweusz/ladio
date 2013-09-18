68346d07c3b4be57c2edf9dc4c83937648c5ede7
Pawel Pelczar <paweusz@gmail.com>
2013-09-18 17:49:57 +0200

    Switch to http-svc-mock for genres-svc

--- angular/test/mock/genres-svc-mock.js
+++ angular/test/mock/genres-svc-mock.js
@@ -1,27 +1,10 @@
 'use strict';
 
-angular.module('ladioApp').factory('GenresSvcMock', function() {
-
-  function SvcRsp() {
-    this.succCb = null;
-    this.errCb = null;
-    this.then = function(succCb, errCb) {
-      this.succCb = succCb;
-      this.errCb = errCb;
-    };
-    this.success = function(succCb) {
-      this.succCb = succCb;
-      return this;
-    };
-    this.error = function(errCb) {
-      this.errCb = errCb;
-      return this;
-    };
-  };
+angular.module('ladioApp').factory('GenresSvcMock', function(HttpSvcMock) {
 
   return {
-    searchSvcRsp: new SvcRsp(),
-    genres: function() {return new SvcRsp();},
+    searchSvcRsp: HttpSvcMock.newHttpPrms(),
+    genres: function() {return HttpSvcMock.newHttpPrms();},
     search: function() {return this.searchSvcRsp;}
   }
 });
