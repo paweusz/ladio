634a70b9260fb8ac28fdf6146ab8b75471a77e63
Pawel Pelczar <paweusz@gmail.com>
2013-09-19 12:04:43 +0200

    Bump versions

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -313,7 +313,7 @@ module.exports = function (grunt) {
         options: {
           variables: {
             'API_URL': 'http://localhost:9001/api',
-            'VERSION': '&alpha;4',
+            'VERSION': '&alpha;5',
             'EMAIL': 'ladioteam@gmail.com'
           },
           force: true
@@ -330,7 +330,7 @@ module.exports = function (grunt) {
         options: {
           variables: {
             'API_URL': 'http://ladio.herokuapp.com/api',
-            'VERSION': '&alpha;4',
+            'VERSION': '&alpha;5',
             'EMAIL': 'ladioteam@gmail.com'
           },
           force: true
