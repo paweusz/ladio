c48ba12a792de59f75ad109261e36e0785c31f16
Pawel Pelczar <paweusz@gmail.com>
2013-09-19 12:02:15 +0200

    Script for showing version numbers

---
+++ bin/show-versions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+grep -H version angular/bower.json
+grep -H version angular/package.json
+grep -H VERSION angular/Gruntfile.js
+grep -H version express/package.json
+grep -H version chrome-ws/manifest.json
